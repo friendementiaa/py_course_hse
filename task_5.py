596c52674ee441681bdcd04175f926a9d6792ecf
friendementiaa <albina.199924@yandex.ru>
2025-11-02 21:10:17 +0300

    Delete task_5.py

--- task_5.py
+++
@@ -1,8 +0,0 @@
-import re
-
-text = "Пост в соцсети: #python #machinelearning @username1 @user_name2"
-# Задача: Найти все упоминания
-# Ожидаемый результат:
-# Упоминания: ['@username1', '@user_name2']
-
-print(re.findall(r'@\w+', text))
