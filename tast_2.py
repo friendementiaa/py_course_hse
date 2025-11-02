01da868ec2e1e035a3cee4b2bd29f2c4808bfbed
friendementiaa <albina.199924@yandex.ru>
2025-11-02 21:10:09 +0300

    Delete tast_2.py

--- tast_2.py
+++
@@ -1,9 +0,0 @@
-import re
-
-text = "Контакты: ivan@mail.ru, petr-ivanov@company.com, info@site.org"
-# Задача: Найти все email-адреса
-# Ожидаемый результат: ['ivan@mail.ru', 'petr-ivanov@company.com', 'info@site.org']
-
-mail_list = re.findall(r'[\w.-]+@[\w.-]+\.\w+', text)
-
-print(mail_list)
